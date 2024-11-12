5b9393d2f9295381218ea475f00301ba3d750efa
Nikhil Reddy <116956174+code-nr@users.noreply.github.com>
2024-11-12 21:27:13 +0530

    Update streamlit_app_mobileprice.py

--- streamlit_app_mobileprice.py
+++ streamlit_app_mobileprice.py
@@ -113,8 +113,8 @@ def prediction(Manufacturer, Category,Screen_Size, CPU,RAM, GPU, OS, Weight,  To
 def main():
     # front end elements of the web page
     html_temp = """
-    <div style ="background-color:yellow;padding:13px">
-    <h1 style ="color:black;text-align:center;"> Pragyan AI Smart Phone Price Prediction ML App</h1>
+    <div style ="background-color:white;padding:13px">
+    <h1 style ="color:black;text-align:center;"> Smart Phone Price Prediction ML App</h1>
     </div>
     """
     # display the front end aspect
